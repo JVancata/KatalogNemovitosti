c3568106e4c091c3776466c2930fb4c4fb3e4689
Jakub Vančata <vancata.kuba@seznam.cz>
2017-10-05 10:22:24 +0200

    Přidejte soubory projektu.

---
+++ Katalog nemovitostí/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katalog_nemovitostí
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
